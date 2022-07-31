5dbec1a7cb7f30af011603ff60ce4131cebd20ad
Sofia <sofia-pomaaltamirano@hotmail.com>
2022-07-30 23:56:29 -0500

    avance

--- Sprint3/product/Ficha.py
+++ Sprint3/product/Ficha.py
@@ -3,9 +3,9 @@ import os
 
 class Ficha:
     sourceFileDir = os.path.dirname(os.path.abspath(__file__))
-    f2 = os.path.join(sourceFileDir, '../static/black.png')
-    f3 = os.path.join(sourceFileDir, '../static/white.png')
-    f4 = os.path.join(sourceFileDir, '../static/high.png')
+    f2 = os.path.join(sourceFileDir, '../../static/black.png')
+    f3 = os.path.join(sourceFileDir, '../../static/white.png')
+    f4 = os.path.join(sourceFileDir, '../../static/high.png')
     def __init__(self):
         self.blackImg = pygame.image.load(self.f2)
         self.whiteImg = pygame.image.load(self.f3)
